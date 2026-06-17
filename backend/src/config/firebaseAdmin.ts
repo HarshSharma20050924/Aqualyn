f1af52d473be36975cfd0c272c754e21d57f439c
Harsh <harsh20050924@gmail.com>
2026-06-18 00:42:07 +0530

    Firebase on backend

--- backend/src/config/firebaseAdmin.ts
+++ backend/src/config/firebaseAdmin.ts
@@ -7,10 +7,11 @@ try {
     if (process.env.FIREBASE_SERVICE_ACCOUNT) {
         serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
     } else {
+        // Firebase Admin SDK REQUIRES snake_case keys
         serviceAccount = {
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+            project_id: process.env.FIREBASE_PROJECT_ID,
+            client_email: process.env.FIREBASE_CLIENT_EMAIL,
+            private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
         };
     }
 } catch (e: any) {
